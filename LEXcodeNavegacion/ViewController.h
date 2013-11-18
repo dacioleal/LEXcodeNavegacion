4465bfbd1c60eaccb3f071089058f4c3e1db9ebc
dacioleal <dacioleal@gmail.com>
2013-11-18 10:31:16 +0100

    Initial Commit

---
+++ LEXcodeNavegacion/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LEXcodeNavegacion
+//
+//  Created by Dacio Leal Rodriguez on 18/11/13.
+//  Copyright (c) 2013 Dacio Leal Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ LEXcodeNavegacionTests/LEXcodeNavegacionTests.m
@@ -0,0 +1,34 @@
+//
+//  LEXcodeNavegacionTests.m
+//  LEXcodeNavegacionTests
+//
+//  Created by Dacio Leal Rodriguez on 18/11/13.
+//  Copyright (c) 2013 Dacio Leal Rodriguez. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LEXcodeNavegacionTests : XCTestCase
+
+@end
+
+@implementation LEXcodeNavegacionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
